7097091ac4f27931263c47ad22aaf5529e046064
Batuhan Günday <batuhan.gunday@icloud.com>
2021-11-14 17:04:19 +0300

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,197 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Batuhan Gunday",
+	title: "Hi all, I'm Batuhan",
+	description:
+		"A passionate Full Stack Web Developer and Mobile App Developer having an experience of building Web applications with TypeScript / Angular / Nodejs / .NET Core and some other cool libraries and frameworks and Cross Platform Mobile Apps With Flutter.",
+	resumeLink: "https://cv.hanzla.ga",
+};
+
+export const openSource = {
+	githubUserName: "BatuhanGnday",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	instagram: "https://www.instagram.com/bateristbatusosu",
+	twitter: "https://twitter.com/batuhangnday",
+	github: "https://github.com/batuhangnday",
+	linkedin: "https://www.linkedin.com/in/batuhangnday/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		emoji(
+			"⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "sass",
+			fontAwesomeClassname: "logos:sass",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		{
+			skillName: "TypeScript",
+			fontAwesomeClassname: "logos:typescript-icon",
+		},
+		{
+			skillName: "reactjs",
+			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+		},
+		{
+			skillName: "nodejs",
+			fontAwesomeClassname: "logos:nodejs-icon",
+		},
+		{
+			skillName: "flutter",
+			fontAwesomeClassname: "logos:flutter",
+		},
+		{
+			skillName: "swift",
+			fontAwesomeClassname: "vscode-icons:file-type-swift",
+		},
+		{
+			skillName: "npm",
+			fontAwesomeClassname: "vscode-icons:file-type-npm",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		{
+			skillName: "aws",
+			fontAwesomeClassname: "logos:aws",
+		},
+		{
+			skillName: "firebase",
+			fontAwesomeClassname: "logos:firebase",
+		},
+		{
+			skillName: "python",
+			fontAwesomeClassname: "logos:python",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+		{
+			skillName: "docker",
+			fontAwesomeClassname: "logos:docker-icon",
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Restful APIs / Backend", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Web Development",
+		progressPercentage: "80",
+	},
+	{
+		Stack: "Mobile Development",
+		progressPercentage: "70",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "University of Turkish Aeronautical Association",
+		subHeader: "Bachelor of Science in Computer Science",
+		duration: "September 2017 - December 2021",
+		desc: "I am a Computer Engineering student who has two courses left to graduate.",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+];
+
+export const experience = [
+	{
+		role: "Full Stack Developer",
+		company: "Larinsoft Information Technology Inc.",
+		companylogo: "/img/icons/common/larinsoft.png",
+		date: "August 2021 – Present",
+		desc: "Develop Responsive Web Applications on Angular with Reactive Approach.",
+		descBullets: [
+			"Flutter",
+			"Angular",
+			".NET Core 5.0",
+		],
+	},
+	{
+		role: "Software Engineer Intern",
+		company: "Bozankaya Automotive Machinery Manufacturing Inc.",
+		companylogo: "/img/icons/common/bozankaya.png",
+		date: "June 2019 – August 2019",
+		desc: "Worken on mostly embedded systems and automation.",
+		descBullets: [
+			"C / C++",
+			"Logic System Design",
+			"Data Structures",
+		]
+	},
+];
+
+export const projects = [
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		link: "https://example.com",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/1hanzla100",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/1hanzla100",
+		link: "https://example.com",
+	},
+];
+
+// export const feedbacks = [
+// 	{
+// 		name: "Hassan Mehmood",
+// 		feedback:
+// 			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Hanzla tauqeer has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
+// 	},
+// 	{
+// 		name: "Maheen Altaf",
+// 		feedback:
+// 			"the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
+// 	},
+// ];
